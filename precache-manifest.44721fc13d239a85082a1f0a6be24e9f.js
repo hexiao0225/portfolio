5e3c5d392c9fca6f154d5a8507c65a8bbcc2a1ca
hexiao0225@gmail.com <xiaohe@MacBook-Pro.local>
2019-10-05 16:19:13 -0700

    Updates

---
+++ precache-manifest.44721fc13d239a85082a1f0a6be24e9f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b3953815c5b199fc0429fa4112b77f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6cadfa73f3f8f5d73e76",
+    "url": "/portfolio/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c4b2cf31e65858fb877d",
+    "url": "/portfolio/static/css/main.df509307.chunk.css"
+  },
+  {
+    "revision": "6cadfa73f3f8f5d73e76",
+    "url": "/portfolio/static/js/2.d5eeb793.chunk.js"
+  },
+  {
+    "revision": "c4b2cf31e65858fb877d",
+    "url": "/portfolio/static/js/main.fe2137f6.chunk.js"
+  },
+  {
+    "revision": "025c9b842f2eed07a387",
+    "url": "/portfolio/static/js/runtime-main.aea3266d.js"
+  },
+  {
+    "revision": "aa1e2efffe2bfa285a16c86ad3ad29cc",
+    "url": "/portfolio/static/media/Mark-Bold.aa1e2eff.ttf"
+  },
+  {
+    "revision": "9a7edf397ed8bfe1049c6c6fae0af48f",
+    "url": "/portfolio/static/media/Mark-Book.9a7edf39.ttf"
+  },
+  {
+    "revision": "ceb9a1022a7d6ca5847f84761c5bfacc",
+    "url": "/portfolio/static/media/Mark-Light.ceb9a102.ttf"
+  },
+  {
+    "revision": "2378620018c8e06eb03c171300645fec",
+    "url": "/portfolio/static/media/Miyake1.23786200.png"
+  },
+  {
+    "revision": "133135c5a7c6ca6b83048f69d1649eb9",
+    "url": "/portfolio/static/media/Miyake2.133135c5.png"
+  },
+  {
+    "revision": "26b29c881707880dcd9ea022710fd8b9",
+    "url": "/portfolio/static/media/Miyake3.26b29c88.png"
+  },
+  {
+    "revision": "f56e11e71d2957916010420742e86367",
+    "url": "/portfolio/static/media/Miyake4.f56e11e7.png"
+  },
+  {
+    "revision": "8f23909dbe5901fbcd559f9adab3d9d5",
+    "url": "/portfolio/static/media/RandomThoughts1.8f23909d.jpg"
+  },
+  {
+    "revision": "c57966adabf72b655832eaf0dbbac94b",
+    "url": "/portfolio/static/media/Recoleta-Bold.c57966ad.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4c84940756203894356dfba81ddaada2.js"
+  "/portfolio/precache-manifest.44721fc13d239a85082a1f0a6be24e9f.js"
 );
 
 self.addEventListener('message', (event) => {
