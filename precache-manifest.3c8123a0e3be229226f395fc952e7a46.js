d16c56dee28be0abc74fbd03eb0875d529a5c351
hexiao0225@gmail.com <xiaohe@MacBook-Pro.local>
2019-10-04 18:13:32 -0700

    Updates

---
+++ precache-manifest.3c8123a0e3be229226f395fc952e7a46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b9b796b628bfb41e71a341998bb66a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "eacad9ea13463f82e592",
+    "url": "/portfolio/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1d15d87d0e1b5dd93f60",
+    "url": "/portfolio/static/js/2.64aa326f.chunk.js"
+  },
+  {
+    "revision": "eacad9ea13463f82e592",
+    "url": "/portfolio/static/js/main.58f6e55f.chunk.js"
+  },
+  {
+    "revision": "025c9b842f2eed07a387",
+    "url": "/portfolio/static/js/runtime-main.aea3266d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio/static/media/logo.25bf045c.svg"
+  }
+]);
